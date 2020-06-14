bea56ba2535a958f8a9645efa5a76435752c6328
呉真 <kuretru@gmail.com>
2020-06-14 18:00:11 +0800

    引入：dmitryshelamov/UI-Cropping-Image

---
+++ src/CroppingImageLibrary/CropToolControl.xaml.cs
@@ -0,0 +1,43 @@
+﻿using CroppingImageLibrary.Services;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace CroppingImageLibrary
+{
+    /// <summary>
+    /// Interaction logic for CropToolControl.xaml
+    /// </summary>
+    public partial class CropToolControl : UserControl
+    {
+        public CropService CropService { get; private set; }
+
+        public CropToolControl()
+        {
+            InitializeComponent();
+        }
+
+        private void RootGrid_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            CropService.Adorner.RaiseEvent(e);
+        }
+
+        private void RootGrid_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            CropService.Adorner.RaiseEvent(e);
+        }
+
+        private void RootGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            CropService = new CropService(this);
+        }
+
+        public void SetImage(BitmapImage bitmapImage)
+        {
+            SourceImage.Source = bitmapImage;
+            // RootGrid.Height = bitmapImage.Height;
+            // RootGrid.Width = bitmapImage.Width;
+        }
+    }
+}
